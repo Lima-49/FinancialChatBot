95f4223643694541351f4f922a9775a1d4e32aea
Lima-49 <vitoraugusto.soares@hotmail.com>
2025-08-15 14:33:15 -0300

    add: config the tools only with gcp tool and change the description of the llm

--- app/services/opena_ai_service.py
+++ app/services/opena_ai_service.py
@@ -2,7 +2,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
-from app.tools.tools import search_tool, wiki_tool, save_tool, gcp_tool
+from app.tools.tools import gcp_tool
 from app.models.schemas import ResearchResponse
 import json
 
@@ -14,7 +14,8 @@ prompt = ChatPromptTemplate.from_messages(
         (
             "system",
             """
-            You are a research assistant that will help generate a research paper.
+            You are a financial agent that will help with the financial questions and
+            Data analysis of data informed.
             Answer the user query and use neccessary tools. 
             Wrap the output in this format and provide no other text\n{format_instructions}
             """,
@@ -25,7 +26,7 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 ).partial(format_instructions=parser.get_format_instructions())
 
-tools = [search_tool, wiki_tool, save_tool, gcp_tool]
+tools = [gcp_tool]
 agent = create_tool_calling_agent(llm=llm, prompt=prompt, tools=tools)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, return_intermediate_steps=True)
 
--- app/tools/tools.py
+++ app/tools/tools.py
@@ -3,38 +3,19 @@ from langchain_community.utilities import WikipediaAPIWrapper
 from langchain_community.tools import Tool
 from google.cloud import bigquery
 from google.oauth2 import service_account
-from datetime import datetime
 from app.core.config import get_env_variable
 import pandas as pd
 
-def save_to_txt(data:str, filename:str = "research_output.txt"):
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    formated_text = f" --- Research Output - {timestamp} --- \n\n{data}\n"
 
-    with open(filename, "a", encoding="utf-8") as file:
-        file.write(formated_text)
+def get_data_from_bigquery(query: str) -> pd.DataFrame:
 
-    return f"Data saved to {filename}"
-
-def get_data_from_bigquery() -> pd.DataFrame:
     prod_credentials = get_env_variable("GCP_SERVICE_ACCOUNT")
     credentials = service_account.Credentials.from_service_account_file(prod_credentials)
     client = bigquery.Client(credentials=credentials)
     query_job = client.query("SELECT * FROM farmbot-436900.CONTROLE_FINANCEIRO.HISTORICO_FINANCEIRO")
-    return query_job.to_dataframe()
+    df = query_job.to_dataframe()
 
-search = DuckDuckGoSearchRun()
-search_tool = Tool(
-    name="SearchDuckDuckGo",
-    func=search.run,
-    description="Search the web using DuckDuckGo. Provide a query to get search results."
-)
-
-save_tool = Tool(
-    name="SaveToTxt",
-    func=save_to_txt,
-    description="Saves the research output to a text file. Provide the data to save."
-)
+    return df
 
 gcp_tool = Tool(
     name="AnalyzeFinancialDataGCP",
